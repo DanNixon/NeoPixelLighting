bf078d7ad84384c7489ed5baac359bdf788c8567
Dan Nixon <dan@dan-nixon.com>
2014-08-27 17:19:13 +0100

    Added high power mode

--- NeoPixelLighting.ino
+++ NeoPixelLighting.ino
@@ -6,6 +6,8 @@
 #define NEOPIXEL_PIN 7
 #define PIXEL_COUNT 144
 
+#define HIGH_POWER_MODE_PIN 12
+
 #define PIXEL_DELAY_TIME 5
 #define MODE_ADDR 0
 
@@ -38,6 +40,9 @@ enum
   BRT
 };
 
+uint8_t mode;
+uint8_t high_power_mode = 0;
+
 void setup() {
 #ifdef SERIAL_BAUD
   Serial.begin(SERIAL_BAUD);
@@ -46,9 +51,14 @@ void setup() {
   strip.begin();
   strip.show();
   
-  uint8_t mode = get_mode();
+  pinMode(HIGH_POWER_MODE_PIN, INPUT_PULLUP);
+  high_power_mode = !digitalRead(HIGH_POWER_MODE_PIN);
+  
+  mode = get_mode();
   
 #ifdef SERIAL_BAUD
+  Serial.print("High power mode: ");
+  Serial.println(high_power_mode);
   Serial.print("Mode: ");
   Serial.println(mode);
 #endif
@@ -84,8 +94,12 @@ uint8_t get_mode()
 
 void set_strip(uint8_t r, uint8_t g, uint8_t b, uint8_t brt)
 {
+  if(high_power_mode)
+    brt = 255;
+    
   strip.setBrightness(brt);
   strip.show();
+  
   for(uint16_t i = 0; i < strip.numPixels(); i++)
   {
     strip.setPixelColor(i, strip.Color(r, g, b));
