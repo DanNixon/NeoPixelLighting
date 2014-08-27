dc104c4fd6fb495f93174702c886d312e75a9f0c
Dan Nixon <dan@dan-nixon.com>
2014-08-27 17:11:30 +0100

    Initial code commit

---
+++ NeoPixelLighting.ino
@@ -0,0 +1,95 @@
+#include <EEPROM.h>
+#include <Adafruit_NeoPixel.h>
+
+#define SERIAL_BAUD 115200
+
+#define NEOPIXEL_PIN 7
+#define PIXEL_COUNT 144
+
+#define PIXEL_DELAY_TIME 5
+#define MODE_ADDR 0
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+#define NUM_MODES 10
+uint8_t modes[NUM_MODES][4] =
+  {
+    {0,   0,   0,   0},
+    
+    {255, 0,   0,   100},
+    {0,   255, 0,   100},
+    {0,   0,   255, 100},
+    
+    {255, 255, 0,   100},
+    {0,   255, 255, 100},
+    {255, 0,   255, 100},
+    
+    {255, 255, 255, 100},
+    {255, 255, 255, 200},
+    
+    {0,   191, 255, 100},
+  };
+
+enum
+{
+  R = 0,
+  G,
+  B,
+  BRT
+};
+
+void setup() {
+#ifdef SERIAL_BAUD
+  Serial.begin(SERIAL_BAUD);
+#endif
+
+  strip.begin();
+  strip.show();
+  
+  uint8_t mode = get_mode();
+  
+#ifdef SERIAL_BAUD
+  Serial.print("Mode: ");
+  Serial.println(mode);
+#endif
+
+  uint8_t *mode_data = modes[mode];
+  
+#ifdef SERIAL_BAUD
+  Serial.print("R=");
+  Serial.println(mode_data[R]);
+  Serial.print("G=");
+  Serial.println(mode_data[G]);
+  Serial.print("B=");
+  Serial.println(mode_data[B]);
+  Serial.print("BRT=");
+  Serial.println(mode_data[BRT]);
+#endif
+
+  set_strip(mode_data[R], mode_data[G], mode_data[B], mode_data[BRT]);
+}
+
+void loop() {
+}
+
+uint8_t get_mode()
+{
+  uint8_t mode = EEPROM.read(MODE_ADDR);
+  mode++;
+  if(mode >= NUM_MODES)
+    mode = 0;
+  EEPROM.write(MODE_ADDR, mode);
+  return mode;
+}
+
+void set_strip(uint8_t r, uint8_t g, uint8_t b, uint8_t brt)
+{
+  strip.setBrightness(brt);
+  strip.show();
+  for(uint16_t i = 0; i < strip.numPixels(); i++)
+  {
+    strip.setPixelColor(i, strip.Color(r, g, b));
+    strip.show();
+    delay(PIXEL_DELAY_TIME);
+  }
+}
--- README.md
+++ README.md
@@ -2,3 +2,7 @@ NeoPixelLighting
 ================
 
 A no frills NeoPixel lighting system
+
+Simple controller for a NeoPixel strip to be used for very basic lighting.
+
+The controller mode is set by reading and incrementing an EEPROM value every power cycle, allowing the mode to be set using the reset button alone thus other than a decoupling capacitor no other components are needed (and even that can be overlooked for serious simplicity).
