aeb158cff10b39ed857442c85835cc69810eab6e
Dan Nixon <dan@dan-nixon.com>
2014-08-27 23:22:52 +0100

    Small style changes

--- NeoPixelLighting.ino
+++ NeoPixelLighting.ino
@@ -17,18 +17,18 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, NEOPIXEL_PIN, NEO_GRB +
 uint8_t modes[NUM_MODES][4] =
   {
     {0,   0,   0,   0},
-    
+
     {255, 0,   0,   100},
     {0,   255, 0,   100},
     {0,   0,   255, 100},
-    
+
     {255, 255, 0,   100},
     {0,   255, 255, 100},
     {255, 0,   255, 100},
-    
+
     {255, 255, 255, 100},
     {255, 255, 255, 200},
-    
+
     {0,   0,   0,   100}
   };
 
@@ -43,21 +43,22 @@ enum
 uint8_t mode;
 uint8_t high_power_mode = 0;
 
-void setup() {
+void setup()
+{
 #ifdef SERIAL_BAUD
   Serial.begin(SERIAL_BAUD);
 #endif
 
   strip.begin();
   strip.show();
-  
+
   // Detect high power mode
   pinMode(HIGH_POWER_MODE_PIN, INPUT_PULLUP);
   high_power_mode = !digitalRead(HIGH_POWER_MODE_PIN);
-  
+
   // Get current mode from EEPROM
   mode = get_mode();
-  
+
 #ifdef SERIAL_BAUD
   Serial.print("High power mode: ");
   Serial.println(high_power_mode);
@@ -67,7 +68,7 @@ void setup() {
 
   // Get data for initial mode state
   uint8_t *mode_data = modes[mode];
-  
+
 #ifdef SERIAL_BAUD
   Serial.print("R=");
   Serial.println(mode_data[R]);
@@ -83,7 +84,8 @@ void setup() {
   set_strip(mode_data[R], mode_data[G], mode_data[B], mode_data[BRT]);
 }
 
-void loop() {
+void loop()
+{
   // Handle dynamic modes
   // Must match index of modes array
   switch(mode)
@@ -95,7 +97,7 @@ void loop() {
       for(j=0; j<256; j++)
       {
         for(i=0; i<strip.numPixels(); i++)
-          strip.setPixelColor(i, Wheel((i+j) & 255));
+          strip.setPixelColor(i, wheel((i+j) & 255));
         strip.show();
         delay(25);
       }
@@ -122,10 +124,10 @@ void set_strip(uint8_t r, uint8_t g, uint8_t b, uint8_t brt)
 {
   if(high_power_mode)
     brt = 255;
-    
+
   strip.setBrightness(brt);
   strip.show();
-  
+
   for(uint16_t i = 0; i < strip.numPixels(); i++)
   {
     strip.setPixelColor(i, strip.Color(r, g, b));
@@ -134,20 +136,20 @@ void set_strip(uint8_t r, uint8_t g, uint8_t b, uint8_t brt)
   }
 }
 
-uint32_t Wheel(byte WheelPos)
+uint32_t wheel(byte wheel_pos)
 {
-  if(WheelPos < 85)
+  if(wheel_pos < 85)
   {
-    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+    return strip.Color(wheel_pos * 3, 255 - wheel_pos * 3, 0);
   }
-  else if(WheelPos < 170)
+  else if(wheel_pos < 170)
   {
-    WheelPos -= 85;
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    wheel_pos -= 85;
+    return strip.Color(255 - wheel_pos * 3, 0, wheel_pos * 3);
   }
   else
   {
-    WheelPos -= 170;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    wheel_pos -= 170;
+    return strip.Color(0, wheel_pos * 3, 255 - wheel_pos * 3);
   }
 }
