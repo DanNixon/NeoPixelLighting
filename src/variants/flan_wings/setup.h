4d24ab017cfcb1460913fff9c887b7bc73fc87ac
Dan Nixon <dan@dan-nixon.com>
2017-03-28 12:33:08 +0100

    add ld modes back

--- src/variants/flan_wings/setup.h
+++ src/variants/flan_wings/setup.h
@@ -8,23 +8,22 @@
 
 void led_setup()
 {
-  StaticColourArray *ca = new StaticColourArray(LED_COUNT);
+  StaticColourArray *wingColours = new StaticColourArray(LED_COUNT);
 
   for (size_t i = 0; i < LED_COUNT; i += 7)
   {
-    ca->setColour(i + 0, 64, 176, 255); // Light blue
-    ca->setColour(i + 1, 0, 0, 255);    // Blue
-    ca->setColour(i + 2, 100, 16, 180); // PUrple
-    ca->setColour(i + 3, 255, 64, 64);  // Pink
-    ca->setColour(i + 4, 255, 100, 0);  // Orange
-    ca->setColour(i + 5, 255, 225, 24); // Yellow
-    ca->setColour(i + 6, 50, 255, 50);  // Light green
+    wingColours->setColour(i + 0, 64, 176, 255); // Light blue
+    wingColours->setColour(i + 1, 0, 0, 255);    // Blue
+    wingColours->setColour(i + 2, 100, 16, 180); // PUrple
+    wingColours->setColour(i + 3, 255, 64, 64);  // Pink
+    wingColours->setColour(i + 4, 255, 100, 0);  // Orange
+    wingColours->setColour(i + 5, 255, 225, 24); // Yellow
+    wingColours->setColour(i + 6, 50, 255, 50);  // Light green
   }
 
-  lighting.addEffect(ca);
-
-  /* lighting.addEffect(new LowPowerSparkle(120)); */
-  /* lighting.addEffect(new Wheel()); */
+  lighting.addEffect(wingColours);
+  lighting.addEffect(new LowPowerSparkle(120));
+  lighting.addEffect(new Wheel());
   /* lighting.addEffect(new StaticColour(255, 0, 0)); */
   /* lighting.addEffect(new StaticColour(0, 255, 0)); */
   /* lighting.addEffect(new StaticColour(0, 0, 255)); */
