6a8f575c15afc64c29833e6858f4cc0a3daa6e2c
Dan Nixon <dan@dan-nixon.com>
2017-03-28 11:53:04 +0100

    clean

--- src/variants/flan_wings/setup.h
+++ src/variants/flan_wings/setup.h
@@ -1,17 +1,18 @@
+#include <effects/LowPowerSparkle.h>
 #include <effects/StaticColour.h>
 #include <effects/Wheel.h>
-#include <effects/LowPowerSparkle.h>
+
 #include <controllers/EEPROMController.h>
 #include <controllers/SerialController.h>
 
 void led_setup()
 {
-  lighting.addEffect(new LowPowerSparkle(120));         // Flash
-  lighting.addEffect(new Wheel());                      // Smooth
-  lighting.addEffect(new StaticColour(255, 0, 0));      // Red
-  lighting.addEffect(new StaticColour(0, 255, 0));      // Green
-  lighting.addEffect(new StaticColour(0, 0, 255));      // Blue
-  lighting.addEffect(new StaticColour(255, 255, 255));  // White
+  lighting.addEffect(new LowPowerSparkle(120));
+  lighting.addEffect(new Wheel());
+  lighting.addEffect(new StaticColour(255, 0, 0));
+  lighting.addEffect(new StaticColour(0, 255, 0));
+  lighting.addEffect(new StaticColour(0, 0, 255));
+  lighting.addEffect(new StaticColour(255, 255, 255));
 
   lighting.addController(new EEPROMController(true));
 }
